342cb46f2aaf04fcf17bb0c0021316f257e9a69d
clw <s0902226835@gmail.com>
2025-10-20 01:34:13 +0800

    add tailwindcss support

--- src/pages/HomePage_58.jsx
+++ src/pages/HomePage_58.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomePage_58 = () => {
   return (
-    <div>
-      <h1>HomePage_58</h1>
-      <h2>clw, 213410458</h2>
+    <div className='flex flex-col justify-center items-center '>
+      <h1 className='text-2xl text-purple-700'>HomePage_58</h1>
+      <h2 className='text-xl'>clw, 213410458</h2>
     </div>
   );
 };
